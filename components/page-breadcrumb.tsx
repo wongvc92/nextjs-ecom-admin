630bd49d4934157e2d3d3205c43a67ca45318ec8
wongvc92 <wongvuichung92@gmail.com>
2024-10-03 22:34:11 +0800

    fix breadcrumb page

--- components/page-breadcrumb.tsx
+++ components/page-breadcrumb.tsx
@@ -3,9 +3,11 @@
 import { useSelectedLayoutSegments } from "next/navigation";
 import Link from "next/link";
 import { ChevronRightIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
 
 export default function PageBreadcrumb() {
   const segments = useSelectedLayoutSegments();
+  const { data } = useSession();
 
   // Filter out segments that are within parentheses like (admin) or (routes)
   const filteredSegments = segments.filter((segment) => !segment.startsWith("(") && !segment.endsWith(")"));
@@ -14,34 +16,38 @@ export default function PageBreadcrumb() {
   const basePath = process.env.NEXT_PUBLIC_APP_URL!;
 
   return (
-    <nav aria-label="Breadcrumb" className="flex items-center space-x-2 text-sm">
-      <Link href={basePath} className="text-gray-600 hover:text-gray-900">
-        Home
-      </Link>
+    <div>
+      {data?.user.id && !data?.user.isBlocked && (
+        <nav aria-label="Breadcrumb" className="flex items-center space-x-2 text-sm">
+          <Link href={basePath} className="text-gray-600 hover:text-gray-900">
+            Home
+          </Link>
 
-      {/* Separator Icon */}
-      {filteredSegments.length > 0 && <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />}
+          {/* Separator Icon */}
+          {filteredSegments.length > 0 && <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />}
 
-      {/* Dynamically generate breadcrumbs based on filtered segments */}
-      {filteredSegments.map((segment, index) => {
-        const path = `${basePath}/${filteredSegments.slice(0, index + 1).join("/")}`;
-        const isLast = index === filteredSegments.length - 1;
+          {/* Dynamically generate breadcrumbs based on filtered segments */}
+          {filteredSegments.map((segment, index) => {
+            const path = `${basePath}/${filteredSegments.slice(0, index + 1).join("/")}`;
+            const isLast = index === filteredSegments.length - 1;
 
-        return (
-          <div key={segment} className="flex items-center space-x-2 text-sm text-ellipsis">
-            {/* Separator Icon */}
-            {index > 0 && <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />}
+            return (
+              <div key={segment} className="flex items-center space-x-2 text-sm text-ellipsis">
+                {/* Separator Icon */}
+                {index > 0 && <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />}
 
-            {isLast ? (
-              <span className="text-gray-500">{decodeURIComponent(segment)}</span>
-            ) : (
-              <Link href={path} className="text-gray-600 hover:text-gray-900">
-                {decodeURIComponent(segment)}
-              </Link>
-            )}
-          </div>
-        );
-      })}
-    </nav>
+                {isLast ? (
+                  <span className="text-gray-500">{decodeURIComponent(segment)}</span>
+                ) : (
+                  <Link href={path} className="text-gray-600 hover:text-gray-900">
+                    {decodeURIComponent(segment)}
+                  </Link>
+                )}
+              </div>
+            );
+          })}
+        </nav>
+      )}
+    </div>
   );
 }
