1ad47a582718f0580955e4e51f5fed2ac20db09f
wongvc92 <wongvuichung92@gmail.com>
2024-10-07 14:54:49 +0800

    add loading skeleton product form

---
+++ app/(admin)/(routes)/products/add-new/loading.tsx
@@ -0,0 +1,8 @@
+import { EditProductFormLoading } from "@/components/loading/edit-product-form-loading";
+import React from "react";
+
+const Loading = () => {
+  return <EditProductFormLoading />;
+};
+
+export default Loading;
