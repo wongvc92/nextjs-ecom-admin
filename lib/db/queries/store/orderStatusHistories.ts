a857a13d6aa626699d20917f6baa2c73a037676c
wongvc92 <wongvuichung92@gmail.com>
2024-10-11 16:29:28 +0800

    fix isr orders

--- lib/db/queries/store/orderStatusHistories.ts
+++ lib/db/queries/store/orderStatusHistories.ts
@@ -1,15 +1,22 @@
+import { unstable_cache } from "next/cache";
 import { db } from "../..";
 import { orderStatusHistories as orderStatusHistoriesTable, OrderStatusHistory } from "../../schema/orderStatusHistory";
 import { asc, eq } from "drizzle-orm";
 
-export const getOrderStatusHistoriesByOrderId = async (orderId: string): Promise<OrderStatusHistory[] | null> => {
-  const orderStatusHistories = await db
-    .select()
-    .from(orderStatusHistoriesTable)
-    .where(eq(orderStatusHistoriesTable.orderId, orderId))
-    .orderBy(asc(orderStatusHistoriesTable.createdAt));
+export const getOrderStatusHistoriesByOrderId = unstable_cache(
+  async (orderId: string): Promise<OrderStatusHistory[] | null> => {
+    const orderStatusHistories = await db
+      .select()
+      .from(orderStatusHistoriesTable)
+      .where(eq(orderStatusHistoriesTable.orderId, orderId))
+      .orderBy(asc(orderStatusHistoriesTable.createdAt));
 
-  if (!orderStatusHistories) return null;
+    if (!orderStatusHistories) return null;
 
-  return orderStatusHistories;
-};
+    return orderStatusHistories;
+  },
+  ["orders"],
+  {
+    tags: ["orders"],
+  }
+);
