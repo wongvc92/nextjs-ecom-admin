0036e8bd4304efa1168437cf7280e21fe40ea866
wongvc92 <wongvuichung92@gmail.com>
2024-10-13 22:38:28 +0800

    fix code

--- app/(admin)/(routes)/categories/components/category-table.tsx
+++ app/(admin)/(routes)/categories/components/category-table.tsx
@@ -13,7 +13,15 @@ interface CategoryTableProps {
 const CategoryTable = async ({ name, perPage, page, dateFrom, dateTo }: CategoryTableProps) => {
   const { categoriesData, categoryCount } = await getCategories(name, perPage, page, dateFrom, dateTo);
 
-  return <DataTable columns={columns} data={categoriesData} totalPage={Math.ceil(categoryCount / parseInt(perPage))} />;
+  return (
+    <DataTable
+      columns={columns}
+      data={categoriesData}
+      totalPage={Math.ceil(categoryCount / parseInt(perPage))}
+      filteredCounts={categoryCount}
+      perPage={perPage}
+    />
+  );
 };
 
 export default CategoryTable;
