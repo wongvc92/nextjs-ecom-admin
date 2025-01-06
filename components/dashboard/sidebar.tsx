c7e7970a3fee235e8911e8877989455d1cf239a8
wongvc92 <wongvuichung92@gmail.com>
2025-01-07 01:39:01 +0800

    hide sidebar for non logged in user

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -3,11 +3,14 @@ import NavLinks from "./nav-links";
 
 const Sidebar = async () => {
   const session = await auth();
-
   return (
-    <div className="top-0 hidden min-h-screen border w-[50px] items-center xl:w-fit xl:flex xl:flex-col pt-10  bg-muted sticky  h-screen  shadow-md dark:bg-transparent">
-      <NavLinks role={session?.user.role as string} />
-    </div>
+    <>
+      {session && session.user.id && (
+        <div className="top-0 hidden min-h-screen border w-[50px] items-center xl:w-fit xl:flex xl:flex-col pt-10  bg-muted sticky  h-screen  shadow-md dark:bg-transparent">
+          <NavLinks role={session?.user.role as string} />
+        </div>
+      )}
+    </>
   );
 };
 
