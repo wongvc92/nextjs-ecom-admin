105d1a4f1e2d7ce6f91c20493ea6fbf76be898ca
wongvc92 <wongvuichung92@gmail.com>
2025-01-06 18:07:56 +0800

    allowed image domain

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,4 +1,4 @@
-export const allowedImageDomains = ["nextjs-dashboard-website.s3.ap-southeast-1.amazonaws.com"];
+export const allowedImageDomains = [process.env.ALLOWED_IMAGE_DOMAIN!];
 
 export const allowedFileTypes = ["image/jpeg", "image/png", "image/heic", "image/heif"];
 
